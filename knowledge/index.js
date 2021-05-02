eb56db14931ab750e3dbd4caf342e79fdb1b6841
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-05-02 14:13:01 +0200

    client practice

--- knowledge/index.js
+++ knowledge/index.js
@@ -2,6 +2,27 @@ const express = require('express');
 
 const app =express();
 
-app.get("/know/:subject",(request,response)=>response.send(`I know${request.params.subject}...`))
+
+
+function rerender(favorite){
+
+
+    const cosa = `<html>
+  <head>
+    <title>Home</title>
+  </head>
+
+  <body>
+    <h1>I know ${favorite}</h1>
+    <p> VIVA</p>
+  </body>
+
+  <style>background:black </style>
+</html>`
+
+return cosa
+}
+
+app.get("/know/:subject",(request,response)=>response.send(rerender(request.params.subject)))
 app.listen(3000,()=>{console.log("listening...")})
 
