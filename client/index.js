7d44c32700870a6b460f148f1ea27ef2d7233549
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-05-03 10:22:32 +0200

    client and server

--- client/index.js
+++ client/index.js
@@ -1,9 +1,30 @@
 const axios = require("axios");
 
+const me = require("./me.json")
+
+ const googleUrl = "yahoo.com";
+
+const people = {
+
+    Spain:"sara",
+    USA:"Mallory",
+    Netherlands:"Merlijn"
+}
+
+console.log(people["Spain"])
 
 
-const googleUrl = "yahoo.com";
+const json=JSON.stringify(people, null, 2)
+const parsedObject= JSON.parse(json);
 
+
+
+// console.log(parsedObject)
+// console.log(people);
+// console.log(json);
+//  console.log(me.likes);
+
+// console.log(me.age);
 async function getUrl(url) {
 
 
@@ -20,3 +41,24 @@ catch(error){
 }
 
 getUrl(googleUrl);
+
+async function getcapital(country){
+ try{
+  const url ="http://localhost:3000/data/capitals"
+  const response = await axios.get(url);
+
+  const {data}=response
+  console.log(data[country])
+  
+
+
+ }
+
+catch(error){
+    console.log(error);
+
+}
+}
+
+
+getcapital("Spain")
